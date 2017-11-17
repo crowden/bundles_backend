08fe16195e35e081a4ce649e1fc46d828d2effbb
crowden <christopherrowden@gmail.com>
2017-11-17 15:03:11 -0600

    ...refactoring backend bundle

---
+++ src/JLibrary/boilerplate/Controller/www/IndexController.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace J29Bundle\Controller\www;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="j29.www.index")
+     */
+    public function indexAction(Request $request){
+        $build = array(
+            'page_title' => 'Welcome!',
+            'page_description' => 'DESCRIPTION',
+        );
+
+        return $this->render('J29Bundle:www:index.html.twig', $build);
+    }
+}
