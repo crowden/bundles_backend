a912b92e8a510fb77f7c2bebd36c879c7891c1f4
crowden <christopherrowden@gmail.com>
2017-11-18 12:27:47 -0600

    ...refactoring backend bundle

---
+++ src/JLibrary/boilerplate/Entity/boilerplate-CrudEntity.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace J29Bundle\Entity\crud;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+/**
+ * Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="crud_entity")
+ */
+
+class Entity
+{
+}
