b60edb4d76b8f90e5c98fc30db5bb78d1a9ad263
crowden <christopherrowden@gmail.com>
2017-11-18 12:32:30 -0600

    ...refactoring backend bundle

---
+++ src/JLibrary/boilerplate/src/Form/crud/boilerplate-EntityType.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace J29Bundle\Form\crud;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder
+            ->add('###')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver){
+        $resolver->setDefaults(array(
+            'data_class' => 'entity_class',
+        ));
+    }
+}
