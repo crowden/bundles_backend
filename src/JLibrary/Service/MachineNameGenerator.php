9d3576d070945c5aba42cdc067a3f0c47b3f2d3c
crowden <christopherrowden@gmail.com>
2017-12-08 21:09:25 -0600

    ...refactoring backend bundle

--- src/JLibrary/Service/MachineNameGenerator.php
+++ src/JLibrary/Service/MachineNameGenerator.php
@@ -9,7 +9,7 @@ class MachineNameGenerator
 
         $no_tags_trimmed_lowercase = strtolower(strip_tags(trim($field_value)));
         $spaces_made_underscores = preg_replace('/( |-)/', '_', $no_tags_trimmed_lowercase);
-        $result = preg_replace('/[^a-z-_]/', '', $spaces_made_underscores);
+        $result = preg_replace('/[^a-z_\-]/', '', $spaces_made_underscores);
 
         if (null !== $prefix) $result = $prefix . '_' . $result;
 
