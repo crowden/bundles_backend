8020c49e2a1e1c6164dcbb1321822d5b99578f93
crowden <christopherrowden@gmail.com>
2017-12-09 18:28:40 -0600

    ...refactoring backend bundle

--- src/JLibrary/Traits/ControllerTraits.php
+++ src/JLibrary/Traits/ControllerTraits.php
@@ -108,7 +108,7 @@ trait ControllerTraits
         return (int)$query_builder->getQuery()->getSingleScalarResult();
     }
 
-    protected function entityDeletionAllowed($repo_namespace, $join_column_property_name)
+    protected function entityDeletionAllowed($repo_namespace, $join_column_property_name){
         $repo = $this->getDoctrine()->getManager()->getRepository($repo_namespace);
 
         $query = $repo->createQueryBuilder('ns')
@@ -116,4 +116,5 @@ trait ControllerTraits
             ->where('ns.' . $join_column_property_name . ' = ' . $entity->getId());
 
         return (int)$query->getQuery()->getSingleScalarResult();
+    }
 }
