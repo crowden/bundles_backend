39ac6787739f8f6b30027a9cd2ada862d7fcdd28
crowden <christopherrowden@gmail.com>
2018-03-05 10:41:44 -0600

    ...refactoring backend bundle

--- src/JLibrary/_boilerplate/Controller/boilerplate-Controller[_WWW_PUBLISHED_].php
+++ src/JLibrary/_boilerplate/Controller/boilerplate-Controller[_WWW_PUBLISHED_].php
@@ -5,6 +5,7 @@ namespace J29Bundle\Controller\**ENTITY_TYPE**;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Twig\Environment;
 
 use J29Bundle\Entity\**ENTITY_TYPE**\**ENTITY**;
 
@@ -13,7 +14,7 @@ class InteriorBasicController extends Controller
     /**
      * @Route("/{urlSlug}", name="j29.**ENTITY_TYPE**.###")
      */
-    public function indexAction(**ENTITY** $entity){
+    public function indexAction(**ENTITY** $entity, Environment $twig){
         $published = $entity->getPublished();
         $admin = $this->isGranted('ROLE_ADMIN');
         
@@ -26,15 +27,12 @@ class InteriorBasicController extends Controller
         ];
         
         // custom templates must match slug in naming
-        $template_custom = 'J29Bundle:**ENTITY_TYPE**/**BASE_TEMPLATE**:' . $entity->getUrlSlug() . '.html.twig';
-        $template_general = 'J29Bundle:**ENTITY_TYPE**:**BASE-TEMPLATE**.html.twig';
-        $template_unauthorized = 'JLibrary:templates/unpublished:unauthorized.html.twig';
-        
-        // dynamically set template
-        $template_to_use = $this
-            ->get('twig')
-            ->getLoader()
-            ->exists($template_custom) ? $template_custom : $template_general;
+        $template_custom = 'J29Bundle:www/news_article:' . $entity->getUrl() . '.html.twig';
+        $template_general = 'J29Bundle:www:news-article.html.twig';
+
+        $template_to_use = $twig->getLoader()->exists($template_custom) ? 
+            $template_custom : 
+            $template_general; 
 
         // entity is published for all to see
         if ($published) return $this->render($template_to_use, $build);
@@ -46,6 +44,6 @@ class InteriorBasicController extends Controller
         }
 
         // trying to view as anonymous
-        if (!$published && !$admin) return $this->render($template_unauthorized);
+        if (!$published && !$admin) throw new AccessDeniedHttpException('Not allowed to view this page');
     }
 }
