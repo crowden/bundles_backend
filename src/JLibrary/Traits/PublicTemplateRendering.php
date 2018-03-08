0b1b35b4149a45afec3d719d87b763dfb69571fa
crowden <christopherrowden@gmail.com>
2018-03-08 15:32:40 -0600

    ...refactoring backend bundle

---
+++ src/JLibrary/Traits/PublicTemplateRendering.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace JLibrary\Traits;
+
+trait PublicTemplateRendering
+{
+    protected function renderByPublishedStatus($entity, $template, $build){
+        // entity is published for all to see
+        if ($entity->getPublished()){
+            $build['notify_unpublished'] = false;
+            return $this->render($template, $build);
+        }
+
+        // entity is unpublished, but they are an admin
+        if (!$entity->getPublished() && $this->isGranted('ROLE_ADMIN')){
+            $build['notify_unpublished'] = true;
+            return $this->render($template, $build);
+        }
+
+        // trying to view as anonymous. Issue 403
+        if (!$entity->getPublished() && !$this->isGranted('ROLE_ADMIN')){
+            throw new AccessDeniedHttpException('Not allowed to view this page');
+        }
+    }
+}
--- src/JLibrary/_boilerplate/Controller/boilerplate-Controller[_WWW_].php
+++ src/JLibrary/_boilerplate/Controller/boilerplate-Controller[_WWW_].php
@@ -6,44 +6,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Twig\Environment;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use JLibrary\Traits\PublicTemplateRendering;
 
 use J29Bundle\Entity\**ENTITY_TYPE**\**ENTITY**;
 
 class InteriorBasicController extends Controller
 {
+    use PublicTemplateRendering;
+    
     /**
      * @Route("/{urlSlug}", name="j29.**ENTITY_TYPE**.###")
      */
     public function indexAction(**ENTITY** $entity, Environment $twig){
-        $published = $entity->getPublished();
-        $admin = $this->isGranted('ROLE_ADMIN');
-        
         // template variables
         $build = [
             'page_description' => $entity->getPageDescription(),
             'page_title' => $entity->getPageTitle(),
             'entity' => $entity,
-            'notify_unpublished' => false,
         ];
         
         // custom templates must match slug in naming
-        $template_custom = 'J29Bundle:www/news_article:' . $entity->getUrl() . '.html.twig';
-        $template_general = 'J29Bundle:www:news-article.html.twig';
+        $template_custom = 'J29Bundle:www/**CUSTOM_FOLDER**:' . $entity->getUrlSlug() . '.html.twig';
+        $template_general = 'J29Bundle:www:**template-name**.html.twig';
 
         $template_to_use = $twig->getLoader()->exists($template_custom) ? 
             $template_custom : 
-            $template_general; 
+            $template_general;
 
-        // entity is published for all to see
-        if ($published) return $this->render($template_to_use, $build);
-
-        // entity is unpublished, but they are an admin
-        if (!$published && $admin){
-            $build['notify_unpublished'] = true;
-            return $this->render($template_to_use, $build);
-        }
-
-        // trying to view as anonymous
-        if (!$published && !$admin) throw new AccessDeniedHttpException('Not allowed to view this page');
+        return $this->renderByPublishedStatus($entity, $template_to_use, $build);
     }
 }
